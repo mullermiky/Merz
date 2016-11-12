18ab0b49eb70bc39fcf03f23d505aced37b1e27c
mulle <mulle@DESKTOP-6FKE0UT>
2016-11-12 10:14:47 +0100

    Add project files.

---
+++ MerzApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MerzApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.listOfFiles = new System.Windows.Forms.ListView();
+            this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menu
+            // 
+            this.menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuLoad,
+            this.menuQuit});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(37, 20);
+            this.menu.Text = "File";
+            // 
+            // menuLoad
+            // 
+            this.menuLoad.Name = "menuLoad";
+            this.menuLoad.Size = new System.Drawing.Size(152, 22);
+            this.menuLoad.Text = "Load";
+            this.menuLoad.Click += new System.EventHandler(this.menuLoad_Click);
+            // 
+            // menuQuit
+            // 
+            this.menuQuit.Name = "menuQuit";
+            this.menuQuit.Size = new System.Drawing.Size(152, 22);
+            this.menuQuit.Text = "Quit";
+            this.menuQuit.Click += new System.EventHandler(this.menuQuit_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // listOfFiles
+            // 
+            this.listOfFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileName});
+            this.listOfFiles.GridLines = true;
+            this.listOfFiles.Location = new System.Drawing.Point(12, 27);
+            this.listOfFiles.Name = "listOfFiles";
+            this.listOfFiles.Size = new System.Drawing.Size(260, 104);
+            this.listOfFiles.TabIndex = 1;
+            this.listOfFiles.UseCompatibleStateImageBehavior = false;
+            this.listOfFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // fileName
+            // 
+            this.fileName.Text = "File Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listOfFiles);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menu;
+        private System.Windows.Forms.ToolStripMenuItem menuLoad;
+        private System.Windows.Forms.ToolStripMenuItem menuQuit;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ListView listOfFiles;
+        private System.Windows.Forms.ColumnHeader fileName;
+    }
+}
+
---
+++ MerzApp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace MerzApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuQuit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void menuLoad_Click(object sender, EventArgs e)
+        {
+            openFile = new OpenFileDialog();
+            Stream stream = null;
+            TextReader reader = null;
+
+            openFile.InitialDirectory = @"c:\";
+            openFile.Filter = "txt files (*.txt)|*.txt|config files (*.config)|*.config|All files *.*|*.*";
+            openFile.FilterIndex = 1;
+            openFile.RestoreDirectory = true;
+
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((stream = openFile.OpenFile()) != null)
+                    {
+                        Debug.WriteLine(stream);
+                        using (stream)
+                        {
+                            using (reader = new StreamReader(stream))
+                            {
+                                String line = reader.ReadLine();
+                                ListViewItem items = new ListViewItem(line);
+                                Debug.WriteLine(line);
+                                items.SubItems.Add(line);
+                                listOfFiles.Items.Add(items);
+                            }
+                        }
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+        }
+    }
+}
